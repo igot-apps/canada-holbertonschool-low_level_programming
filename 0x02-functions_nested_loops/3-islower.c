9a5e7d6cda06e6668be34987e66be15fae2fd608
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-16 11:24:19 -0700

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * _islower - tests whether a character is a
- * lowercase letter from the English alphabet.
- * @c: character to test.
- * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
+ * _islower - tests whetheracharacteris a
+ * lowercase letter from theEnglishalphabet.
+ * @c: character totest.
+ * Return: 1 if it is alowercase letter, 0 ifitis notalowercase letter.
  */
 int _islower(int c)
 {
