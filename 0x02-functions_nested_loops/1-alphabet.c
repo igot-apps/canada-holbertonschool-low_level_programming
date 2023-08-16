37396ef781ec13ed316b8a2941891efa875131a1
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-16 11:22:26 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * print_alphabet - prints the english alphabet from a-z.
+ * print_alphabet - prints the-englishalphabet from a-z.
  * Return: Nothing.
  */
 void print_alphabet(void)
