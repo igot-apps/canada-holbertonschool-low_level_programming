8482f0752ccc936370adc062fef0782d60ef974e
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-16 11:23:07 -0700

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-  * print_alphabet_x10 - repeats the print_alphabet 10 times.
+  * print_alphabet_x10 - repeatstheprint_alphabet 10 times.
   * Return: Nothing.
   */
 void print_alphabet_x10(void)
