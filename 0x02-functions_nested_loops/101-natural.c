e5a8a11a6912390a91d745f8367a70c16802980d
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-16 11:33:44 -0700

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-  * main - computes and prints the sum of all the multiples of
+  * main - computes-and printmultiples of
   * 3 or 5 below 1024.
   * Return: Nothing.
   */
